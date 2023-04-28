f34f9fe417a7455dd9cb6a84b16be5d73d1aa11d
Mohammed Rahman <minhaz357@gmail.com>
2023-04-27 21:36:21 -0400

    Update BookListComponent to get the list of books

--- src/app/components/book-list/book-list.component.ts
+++ src/app/components/book-list/book-list.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Book } from 'src/app/common/book';
+import { BookService } from 'src/app/services/book.service';
 
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
-  styleUrls: ['./book-list.component.css']
+  styleUrls: ['./book-list.component.css'],
 })
-export class BookListComponent {
+export class BookListComponent implements OnInit {
+  // create books property
+  books: Book[] = [];
+  // inject BookService
+  constructor(private bookService: BookService) {}
 
+  ngOnInit(): void {
+    this.listBooks();
+  }
+  listBooks() {
+    // method is invoked when we subscribe
+    this.bookService.getBookList().subscribe((data) => {
+      this.books = data; // assign results to the books array
+    });
+  }
 }

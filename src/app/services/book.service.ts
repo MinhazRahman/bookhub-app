a528e42616a8530e1027efc55cfd816cc5024f62
Mohammed Rahman <minhaz357@gmail.com>
2023-04-27 21:32:28 -0400

    Map the JSON data from Spring Data REST to Book array
    	- create getBookList() method

--- src/app/services/book.service.ts
+++ src/app/services/book.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Book } from '../common/book';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BookService {
+  private baseUrl = 'http://localhost:8080/bookhub/books';
 
-  constructor() { }
+  // inject HttpClient
+  constructor(private httpClient: HttpClient) {}
+
+  // Maps the JSON data from Spring Data REST to Book array
+  getBookList(): Observable<Book[]> {
+    return this.httpClient.get<Book[]>(this.baseUrl);
+  }
 }

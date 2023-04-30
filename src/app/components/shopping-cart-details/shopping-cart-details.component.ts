a09e84aa3b26de2402559b31981feca919fc7d12
Mohammed Rahman <minhaz357@gmail.com>
2023-04-30 16:52:19 -0400

    Modify ShoppingCartDetailsComponent to retrieve shopping cart items

--- src/app/components/shopping-cart-details/shopping-cart-details.component.ts
+++ src/app/components/shopping-cart-details/shopping-cart-details.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ShoppingCartItem } from 'src/app/common/shopping-cart-item';
+import { ShoppingCartService } from 'src/app/services/shopping-cart.service';
 
 @Component({
   selector: 'app-shopping-cart-details',
   templateUrl: './shopping-cart-details.component.html',
-  styleUrls: ['./shopping-cart-details.component.css']
+  styleUrls: ['./shopping-cart-details.component.css'],
 })
-export class ShoppingCartDetailsComponent {
+export class ShoppingCartDetailsComponent implements OnInit {
+  shoppingCartItems: ShoppingCartItem[] = [];
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
 
+  // inject shoppingCartService
+  constructor(private shoppingCartService: ShoppingCartService) {}
+
+  ngOnInit(): void {
+    this.listShoppingCartDetails();
+  }
+
+  listShoppingCartDetails() {
+    // get a handle to the shopping cart items
+    this.shoppingCartItems = this.shoppingCartService.shoppingCartItems;
+
+    // subscribe to the totalPrice
+    this.shoppingCartService.totalPrice.subscribe(
+      (data) => (this.totalPrice = data)
+    );
+
+    // subscribe to the totalQuantity
+    this.shoppingCartService.totalQuantity.subscribe(
+      (data) => (this.totalQuantity = data)
+    );
+
+    // calculate shopping cart total price and quantity
+    this.shoppingCartService.calculateShoppingCartTotals();
+  }
 }

232bcbe555572d8ee83fdd93bd9188457d01aefb
Mohammed Rahman <minhaz357@gmail.com>
2023-04-30 13:12:06 -0400

    Update ShoppingCartStatusComponent to subscribe to ShoppingCarService events
    	- subscribe to totalPrice
    	- subscribe to totalQuantity

--- src/app/components/shopping-cart-status/shopping-cart-status.component.ts
+++ src/app/components/shopping-cart-status/shopping-cart-status.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ShoppingCartService } from 'src/app/services/shopping-cart.service';
 
 @Component({
   selector: 'app-shopping-cart-status',
   templateUrl: './shopping-cart-status.component.html',
-  styleUrls: ['./shopping-cart-status.component.css']
+  styleUrls: ['./shopping-cart-status.component.css'],
 })
-export class ShoppingCartStatusComponent {
+export class ShoppingCartStatusComponent implements OnInit {
+  totalPrice: number = 0.0;
+  totalQuantity: number = 0;
 
+  constructor(private shoppingCartService: ShoppingCartService) {}
+
+  ngOnInit(): void {
+    this.updateShoppingCartStatus();
+  }
+
+  updateShoppingCartStatus() {
+    // subscribe for events
+    // subscribe to the shopping cart totalPrice
+    this.shoppingCartService.totalPrice.subscribe(
+      (data) => (this.totalPrice = data)
+    );
+
+    // subscribe to the shopping cart totalQuantity
+    this.shoppingCartService.totalQuantity.subscribe(
+      (data) => (this.totalQuantity = data)
+    );
+  }
 }

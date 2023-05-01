f63a328867129f2e3c97fa0579ec07533390a5b6
Mohammed Rahman <minhaz357@gmail.com>
2023-05-01 15:11:30 -0400

    Update CheckoutComponent to retieve the credit card months and years

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { UtilityFormService } from 'src/app/services/utility-form.service';
 
 @Component({
   selector: 'app-checkout',
@@ -13,8 +14,14 @@ export class CheckoutComponent implements OnInit {
   totalPrice: number = 0;
   totalQuantity: number = 0;
 
+  creditCardMonths: number[] = [];
+  creditCardYears: number[] = [];
+
   // inject the form builder
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private utilityFormService: UtilityFormService
+  ) {}
 
   ngOnInit(): void {
     // build the form
@@ -50,6 +57,19 @@ export class CheckoutComponent implements OnInit {
         expirationYear: [''],
       }),
     });
+
+    // populate credit card months
+    const startMonth: number = new Date().getMonth() + 1; // months are 0-based that's why add 1 to the month
+    // call getCreditCardMonths() method, subscribe to the call and initialize creditCardMonths
+    this.utilityFormService
+      .getCreditCardMonths(startMonth)
+      .subscribe((data) => (this.creditCardMonths = data));
+
+    // populate credit card years
+    const startYear: number = new Date().getFullYear();
+    this.utilityFormService
+      .getCreditCardYears()
+      .subscribe((data) => (this.creditCardYears = data));
   }
 
   onSubmit() {

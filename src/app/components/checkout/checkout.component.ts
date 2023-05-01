9339589b90834c7c3e80eea3a75b255435ad1b4f
Mohammed Rahman <minhaz357@gmail.com>
2023-04-30 21:08:20 -0400

    Define form in checkout.component.ts file

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
-  styleUrls: ['./checkout.component.css']
+  styleUrls: ['./checkout.component.css'],
 })
-export class CheckoutComponent {
+export class CheckoutComponent implements OnInit {
+  // declare form group
+  checkoutFormGroup!: FormGroup;
 
+  // inject the form builder
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    // build the form
+    this.checkoutFormGroup = this.formBuilder.group({
+      customer: this.formBuilder.group({
+        firstName: [''],
+        lastName: [''],
+        email: [''],
+      }),
+    });
+  }
 }

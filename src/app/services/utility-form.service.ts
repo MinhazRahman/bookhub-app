17ce38810bbf6dfd74155e5d923a89dbdcc7ad2c
Mohammed Rahman <minhaz357@gmail.com>
2023-05-01 11:27:17 -0400

    Add mothods to UtilityForService to get months and years

--- src/app/services/utility-form.service.ts
+++ src/app/services/utility-form.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UtilityFormService {
+  constructor() {}
 
-  constructor() { }
+  getCreditCardMonths(startMonth: number): Observable<number[]> {
+    let data: number[] = [];
+
+    // build an array for Month dropdown list
+    // start at the current month and loop until 12
+    for (let theMonth = startMonth; theMonth <= 12; theMonth++) {
+      data.push(theMonth);
+    }
+
+    // 'of' operator from 'rxjs' will wrap 'data' as an Observable
+    return of(data);
+  }
+
+  getCreditCardYears(): Observable<number[]> {
+    let data: number[] = [];
+
+    // build an array for Year dropdown list
+    // start at the current year and loop through 10 next year
+    let startYear: number = new Date().getFullYear();
+    let endYear: number = startYear + 10;
+
+    for (let theYear = startYear; theYear <= endYear; theYear++) {
+      data.push(theYear);
+    }
+
+    return of(data);
+  }
 }

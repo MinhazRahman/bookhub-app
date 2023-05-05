f7d65f8bdaedd40d3850583fd446387c2e24717a
Mohammed Rahman <minhaz357@gmail.com>
2023-05-04 23:46:28 -0400

    Create placeOrder method
    	- The above TypeScript function placeOrder is a method that
    	- sends an HTTP POST request to the server to place an order for a purchase.
    	- It takes in a Purchase object as an argument and returns an Observable of any type.
    
    	```
    	- The httpClient.post method is used to send an HTTP POST request to the server with the specified URL
    	- and the purchase object as the body of the request.
    	- The purchaseUrl variable represents the URL where the purchase request will be sent.
    	```

--- src/app/services/checkout.service.ts
+++ src/app/services/checkout.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Purchase } from '../common/purchase';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CheckoutService {
+  private purchaseUrl = 'http://localhost:8080/bookhub/checkout/purchase';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+
+  /*
+   * placeOrder method sends an HTTP POST request to the server to place an order for a purchase.
+   * It takes in a Purchase object as an argument and returns an Observable of any type.
+   */
+  placeOrder(purchase: Purchase): Observable<any> {
+    return this.httpClient.post<Purchase>(this.purchaseUrl, purchase);
+  }
 }

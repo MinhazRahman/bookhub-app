8a6424f182778e8600c09fb40109d42acdb84cc1
Mohammed Rahman <minhaz357@gmail.com>
2023-05-08 21:00:54 -0400

    Develop OrderHistoryService to get the order history
    	- Add getOrderHistory() method

--- src/app/services/order-history.service.ts
+++ src/app/services/order-history.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Order } from '../common/order';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class OrderHistoryService {
+  private orderUrl = 'http://localhost:8080/bookhub/orders';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+
+  getOrderHistory(customerEmail: String): Observable<GetResponseOrderHistory> {
+    // build the url based of customer email
+    const orderHistoryUrl = `${this.orderUrl}/findByCustomerEmail/${customerEmail}`;
+    return this.httpClient.get<GetResponseOrderHistory>(orderHistoryUrl);
+  }
+}
+
+// extract the JSON array from the Spring data REST json response
+interface GetResponseOrderHistory {
+  content: Order[];
 }

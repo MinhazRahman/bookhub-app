567d729003a9793729a228c08d5e24c262e5d023
Mohammed Rahman <minhaz357@gmail.com>
2023-06-03 06:09:04 -0400

    Create AuthInterceptor and handle the access

---
+++ src/app/services/auth-interceptor.service.ts
@@ -0,0 +1,40 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { OKTA_AUTH } from '@okta/okta-angular';
+import { OktaAuth } from '@okta/okta-auth-js';
+import { Observable, from, lastValueFrom } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthInterceptorService implements HttpInterceptor {
+
+  constructor(@Inject(OKTA_AUTH) private oktaAuth: OktaAuth) { }
+  
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return from(this.handleAccess(request, next));
+  }
+
+  private async handleAccess(request: HttpRequest<any>, next: HttpHandler): Promise<HttpEvent<any>> {
+    // add access tokens only for secured endpoints
+    const securedEndpoints = ['http://localhost:8080/bookhub/orders'];
+    
+    if (securedEndpoints.some(url => request.urlWithParams.includes(url))) { 
+      // get the access token
+      const accessToken = this.oktaAuth.getAccessToken();
+
+      // since request is immutable, we have to clone the request and then add the headers
+      // clone the request and add new header with access token
+      request = request.clone({
+        setHeaders: {
+          Authorization: 'Bearer ' + accessToken
+        }
+      });
+    }
+
+    return await lastValueFrom(next.handle(request));
+  }
+}

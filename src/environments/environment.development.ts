bb72adc171df77522d5434cdb07d60dfc25ec6c1
Mohammed Rahman <minhaz357@gmail.com>
2023-06-04 04:04:50 -0400

    Add Stripe Publishable key

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
   bookHubApiUrl: 'https://localhost:8443/bookhub',
+  stripePublishableKey:
+    'pk_test_51NF69nFNu0IcLItT5JpRUuwrafQAnbZ7vvvQbtsXK7YCifz73mg9RlJk2NHHTQnPBcTrBOTVQEGWclunicJBpCxI00MS9Ezc8A',
 };

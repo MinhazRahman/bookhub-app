78168a04235655cc3fbf12234f9d77cb79a368cc
Mohammed Rahman <minhaz357@gmail.com>
2023-06-04 05:15:18 -0400

    Update CheckoutService to create a PaymentIntent

--- src/app/services/checkout.service.ts
+++ src/app/services/checkout.service.ts
@@ -3,12 +3,14 @@ import { Injectable } from '@angular/core';
 import { Purchase } from '../common/purchase';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { PaymentInfo } from '../common/payment-info';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CheckoutService {
   private purchaseUrl = environment.bookHubApiUrl + '/checkout/purchase';
+  private paymentIntentUrl = environment.bookHubApiUrl + '/checkout/payment-intent';
 
   constructor(private httpClient: HttpClient) {}
 
@@ -18,5 +20,9 @@ export class CheckoutService {
    */
   placeOrder(purchase: Purchase): Observable<any> {
     return this.httpClient.post<Purchase>(this.purchaseUrl, purchase);
+  }
+
+  createPaymentInfo(paymentInfo: PaymentInfo): Observable<any> { 
+    return this.httpClient.post<PaymentInfo>(this.paymentIntentUrl, paymentInfo);
   }
 }
